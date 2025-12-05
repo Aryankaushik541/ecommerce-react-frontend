b407f55f1167a9e69c2bc9caaf142e83c3f33f0b
Aryankaushik541 <Aryankaushik541@gmail.com>
2025-12-05 22:10:51 +0530

    Add PrivateRoute component

---
+++ src/components/PrivateRoute.js
@@ -0,0 +1,19 @@
+import React, { useContext } from 'react';
+import { Navigate } from 'react-router-dom';
+import { AuthContext } from '../context/AuthContext';
+
+const PrivateRoute = ({ children }) => {
+  const { user } = useContext(AuthContext);
+
+  if (!user) {
+    return <Navigate to="/login" />;
+  }
+
+  if (!user.is_staff) {
+    return <Navigate to="/" />;
+  }
+
+  return children;
+};
+
+export default PrivateRoute;
